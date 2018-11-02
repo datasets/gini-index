00ed7d4842c8d3cd0e8e8c3fbaab7e3874a99d19
Branko Djordjevic <brankodj89@gmail.com>
2018-11-02 02:16:49 +0100

    [xs][process]: Fixed bugs and syntax

--- scripts/process.py
+++ scripts/process.py
@@ -13,10 +13,10 @@ print(source)
 
 def main():
     giniIndex = pd.read_csv(source)
-    giniIndex.to_csv('archive/gini-index.csv', sep=",", index_col=0, index=False) 
+    giniIndex.to_csv('archive/gini-index.csv', sep=",", index=False)
     print("Saved archive CSV file.")
-    print (giniIndex)
-    
+    print(giniIndex)
+
     # Processing the data
     df = pd.read_csv('archive/gini-index.csv')      # Reading the source csv
     """
@@ -24,9 +24,9 @@ def main():
     work for the remaining code
     """
     df.columns.values[0] = 'Country Name'
-    
+
     df = pd.melt(df, id_vars=['Country Name', 'Country Code'], var_name="Year", value_name="Value")     # Unpivoting
-    df = df.sort_values(by=['Country Name', 'Year'], ascending=[True, True]) # Sorting by country
+    df = df.sort_values(by=['Country Name', 'Year'], ascending=[True, True])  # Sorting by country
 
     df.dropna().to_csv('data/gini-index.csv', sep=",", index=False)   # Saving CSV
     print ("File has been saved and it is ready for data packaging.")
